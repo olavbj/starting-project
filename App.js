9646f5f8988ca89ebd634d1f5b37e8d18d99084b
Olav <olav@cerex.no>
2023-10-23 22:37:38 +0200

    first update

--- App.js
+++ App.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
+       <Text style={[styles.text, styles.whiteText, styles.largeText]}>Spring</Text>
+      <Text style={[styles.text, styles.smallText]}>Summer</Text>
+      <View style={{ height: 30 }}></View>
+      <Text style={[styles.text, styles.mediumText]}>Autumn</Text>
+      <Text style={[styles.text, styles.whiteText, styles.largeText]}>Winter</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#003540',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  smallText:  {
+    fontSize: 20,
+  },
+  mediumText: {
+    fontSize: 24,
+  },
+  largeText: {
+    fontSize: 34,
+
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  
 });
